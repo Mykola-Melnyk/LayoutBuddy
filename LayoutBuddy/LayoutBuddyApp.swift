18e85f5b56870dc6eddbf8c04ca9348c612f49b1
mykola-melnyk <mr.nicholas.x@gmail.com>
2025-08-10 09:48:40 +0300

    Initial Commit

---
+++ LayoutBuddy/LayoutBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutBuddyApp.swift
+//  LayoutBuddy
+//
+//  Created by Mykola Melnyk on 10.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct LayoutBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LayoutBuddyTests/LayoutBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutBuddyTests.swift
+//  LayoutBuddyTests
+//
+//  Created by Mykola Melnyk on 10.08.2025.
+//
+
+import Testing
+@testable import LayoutBuddy
+
+struct LayoutBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
