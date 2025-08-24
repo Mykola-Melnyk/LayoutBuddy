0a7191ff6af9ca5848bfa9c955862e2ab1fa0e68
Mykola Melnyk <108155015+Mykola-Melnyk@users.noreply.github.com>
2025-08-24 01:46:32 -0700

    Create ReplacementPerformer.swift

---
+++ Refactor/Services/ReplacementPerformer.swift
@@ -0,0 +1,15 @@
+import AppKit
+
+enum ReplacementError: Error {
+  case unavailableAX(String)
+  case setFailed
+}
+
+@MainActor
+protocol ReplacementPerformer {
+  func replace(in element: AXUIElement, range: TextRange, with text: String) async throws
+  func beginSynthesis()
+  func endSynthesis()
+  func synthesizeDeletion(count: Int)
+  func synthesizeInsertion(_ text: String)
+}
